ac62e8d7599e6fbf688fb7e51a2e19221fa6abfe
Superpure <felipe.aguilera@alumnos.ucn.cl>
2021-12-31 19:49:39 -0300

    Productos creados

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+import Productos from './components/Productos'
 import './App.css';
 
 class App extends Component {
   state = {
     productos: [
-      {name: 'Tomate', price: 1500, img = "/productos/tomate.jpg"},
-      {name: 'Arbejas', price: 2500, img = "/productos/arbejas.jpg"},
-      {name: 'Lechuga', price: 500, img = "/productos/lechuga.jpg"}
+      {name: 'Tomate', price: 1500, img: "public/productos/tomate.jpg"},
+      {name: 'Arbejas', price: 2500, img: "public/productos/arbejas.jpg"},
+      {name: 'Lechuga', price: 500, img: "public/productos/lechuga.jpg"},
     ]
   }
   render(){
     return (
       <div>
-        <p>Hola Mundo</p>
+        <Productos 
+          agregarAlCarro={ () => console.log('No hace nada') }
+          productos={this.state.productos}
+        />
       </div>
     )
   }
